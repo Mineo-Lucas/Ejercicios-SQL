c68eb3f7fd33f59d2579de126106c145e5a23e8c
unknown <lucas_mineo@outlook.com>
2024-05-06 14:13:25 -0300

    ejercicios parte 2

--- ejercicos SQL.sql
+++ ejercicos SQL.sql
@@ -46,3 +46,39 @@ select COUNT(Id) 'cantidad peliculas' from Peliculas
 
 --ejercicio 10
 select * from Usuarios where Activo=0
+
+--ejercicio 11
+select * from Peliculas
+insert into Peliculas values('1968-11-1','"The Good, the Bad and the Ugly"',162,
+'"Tres hombres violentos pelean por una caja que alberga 200 000 dólares, la cual fue escondida durante la Guerra Civil. 
+Dado que ninguno puede encontrar la tumba donde está el botín sin la ayuda de los otros dos, deben colaborar, pese a odiarse."',null)
+
+--ejercicio 12
+select * from Usuarios
+select * from [Usuarios.Favoritos]
+insert into [Usuarios.Favoritos] values(30,4,'2024-05-06')
+
+--ejercicio 13
+select * from Plataformas
+select * from [Peliculas.Plataformas]
+insert into [Peliculas.Plataformas](Id,IdPelicula,IdPlataforma,FechaAlta) values(44,30,1,'2024-05-06')
+insert into [Peliculas.Plataformas](Id,IdPelicula,IdPlataforma,FechaAlta) values(45,30,2,'2024-05-06') 
+
+--ejercicio 14(no hay que hacer nada en la db)
+
+--ejercicio 15
+select * from Plataformas
+select * from [Peliculas.Plataformas]
+select * from Peliculas
+update [Peliculas.Plataformas] set FechaBaja='2024-01-16' where Id=5
+
+--ejercicio 16()
+select * from Peliculas
+select * from Reparto where Nombre='robert'
+select * from [Peliculas.Reparto]
+update [Peliculas.Reparto] set IdReparto=107 where Id=11
+
+
+
+
+
